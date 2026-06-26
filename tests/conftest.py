a953ec2d4c00ca9d2a73d5cf766621920105c3ae
jiya yadav <jiyayadav102@gmail.com>
2026-06-26 23:56:02 +0545

    test(infra): add pytest config + shared fixtures

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "8b25607d-2ae1-481f-9c4a-4fb98cbd00b5",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
